a83b7b50542e0e70f22db37b88fd76429e586039
Jason Vernot <jason.vernot@gmail.com>
2020-07-08 16:55:23 -0400

    updated api. removed unused files and calls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/header";
-import Navbar from "./components/Navbar/navbar";
 import Wrapper from "./components/Wrapper/wrapper";
 import UserTable from "./components/UserTable/userTable";
-import UserContext from "./utils/userContext";
 
 function App() {
   return (
